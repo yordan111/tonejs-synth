b0b9ccb62bd51a0fed0f1637cdc9d730c9218813
Yordan Chorukov <Yordan.Chorukov@stud.srh-university.de>
2026-01-14 19:10:42 +0100

    script.js

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
 let synth;
 
 document.getElementById("start").onclick = async () => {
-  await Tone.start();
+  await Tone.start(); // Unlocks audio in the browser
   console.log("Audio started");
 
-  synth = new Tone.Synth().toDestination();
-  synth.triggerAttack("C4");
+  if (!synth) {
+    synth = new Tone.Synth().toDestination();
+  }
+
+  synth.triggerAttack("C4"); // Play note continuously
+};
+
+document.getElementById("stop").onclick = () => {
+  if (synth) {
+    synth.triggerRelease(); // Stop note
+  }
 };
 
 document.getElementById("freq").oninput = (e) => {
   if (synth) {
-    synth.oscillator.frequency.value = e.target.value;
+    synth.oscillator.frequency.value = e.target.value; // Real-time pitch control
   }
 };
+
